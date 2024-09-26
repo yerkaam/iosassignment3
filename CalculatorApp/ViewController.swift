c8764b7a5c3957928313830a0498bc49caca8f26
yerkaam <eraorynbai@gmail.com>
2024-09-26 16:34:07 +0500

    Initial Commit

---
+++ CalculatorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalculatorApp
+//
+//  Created by Yerdaulet Orynbay on 26.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
