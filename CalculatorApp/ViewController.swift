47a64bc061f4ad65837e1f3edec9ce7e4d624455
yerkaam <eraorynbai@gmail.com>
2024-10-03 14:35:15 +0500

    first commit

--- CalculatorApp/ViewController.swift
+++ CalculatorApp/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+ 
 
+
+    @IBOutlet var Buttons: [UIButton]!
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+            super.viewDidLoad()
+            updateButtonLayout(isLandscape: UIDevice.current.orientation.isLandscape)
+        }
+
+        override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+            super.viewWillTransition(to: size, with: coordinator)
+            
+            // Определяем, является ли ориентация горизонтальной (landscape)
+            let isLandscape = size.width > size.height
+            
+            // Адаптируем кнопки при повороте экрана
+            coordinator.animate(alongsideTransition: { _ in
+                self.updateButtonLayout(isLandscape: isLandscape)
+            })
+        }
+
+        func updateButtonLayout(isLandscape: Bool) {
+            for button in Buttons {
+                if isLandscape {
+                    // Настройка кнопок для альбомной ориентации (landscape)
+                    button.layer.cornerRadius = 20
+                    button.titleLabel?.font = UIFont.systemFont(ofSize: 15)
+                } else {
+                    // Настройка для портретной ориентации
+                    button.layer.cornerRadius = 40 
+                    button.titleLabel?.font = UIFont.systemFont(ofSize: 35)// Восстановить круглый вид
+                   
+                }
+            }
+        }
     }
-
-
-}
 
